95b115b190392b2bb4977a31118f4061c416471a
cherianbiju <cherianbiju52@gmail.com>
2026-06-02 19:28:45 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Cherian Biju',
+    author_email= 'cherianbiju52@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
